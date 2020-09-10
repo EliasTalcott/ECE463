0e360e0d1458fb77bb1552e728f95b635b40c3ca
Elias Matthew Talcott <etalcott@ecegrid-thin1.ecn.purdue.edu>
2020-09-10 19:56:18 -0400

    Solved lab 1 part 1

--- Lab1/Part1/httpclient.etalcott.c
+++ Lab1/Part1/httpclient.etalcott.c
@@ -6,7 +6,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAXLINE 100
+#define MAXLINE 1024
+#define MAXREQUEST 512
 
 int open_clientfd(char * hostname, int port);
 
@@ -40,23 +41,77 @@ int open_clientfd(char * hostname, int port) {
 int main(int argc, char ** argv) {
 	// Declare client variables and buffers
 	int clientfd, port;
-	char * host, buf[MAXLINE];
+	char * host;
+	char buf[MAXLINE];
+	char * path;
+	char filename[MAXREQUEST];
 
 	// Initialize client variables and buffers
 	host = argv[1];
 	port = atoi(argv[2]);
+	path = argv[3];
 
-	// Open connection and GET filename
+
+	// Open first connection and GET filename
 	clientfd = open_clientfd(host, port);
 	if (clientfd < 0) {
 		printf("Error opening connection\n");
 		exit(0); 
 	}
-	
+	// Compose and send request
+	char message[MAXREQUEST];
+	snprintf(message, sizeof(message), "GET %s HTTP/1.0\r\n\r\n", path);
+	send(clientfd, message, strlen(message), 0);
+	// Receive response, check for code 200, and send server response to stdout
+	recv(clientfd, buf, MAXLINE - 1, 0);
+	if (strstr(buf, "200 OK") != NULL) {
+		int length = strlen(buf);
+		int n = 0;
+		int num_newlines = 0;
+		while (num_newlines < 12) {
+			if (buf[n] == '\n') {
+				num_newlines++;
+			}
+			putchar(buf[n]);
+			n++;
+		}
+	} else {
+		printf("First GET request did not return code 200");
+		return(0);
+	}
+
+	// Extract filename from server response
+	int length = strlen(buf);
+	int i = 0;
+	int n = 1;
+	char b = buf[n - 1]; 
+	char c = buf[n];
+	while (b != '\n' || c != '\r') {
+		n++;
+		b = buf[n - 1];
+		c = buf[n];
+	}
+	n += 2;
+	i = 0;
+	while (buf[n] != '\n') {
+		filename[i] = buf[n];
+		i++;
+		n++;
+	}
+
 	// Reopen connection and GET file contents
 	clientfd = open_clientfd(host, port);
 	if (clientfd < 0) {
 		printf("Error opening connection\n");
 		exit(0); 
+	}
+	// Compose and send request
+	bzero(message, MAXREQUEST);
+	snprintf(message, MAXREQUEST, "GET %s HTTP/1.0\r\n\r\n", filename);
+	send(clientfd, message, strlen(message), 0);
+	// Receive and send server response to stdout in chunks of size MAXLINE
+	while (recv(clientfd, buf, MAXLINE - 1, 0) != 0) {
+		puts(buf);
+		bzero(buf, MAXLINE);
 	}
 }
